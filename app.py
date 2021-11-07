c9a8a507875f3c8d536c36bc9f434094c7b4be64
Bacon <85359439+AmberBacon@users.noreply.github.com>
2021-11-06 20:25:31 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import models 
+# main menu - add, search, analysis, exit, view
+# add book to the database
+# edit books
+# delete books
+# search books
+# data cleaning 
+# loop runs program 
---
+++ models.py
@@ -0,0 +1,4 @@
+# create a database
+# books.db
+# create a model
+# title, author, date published, price
