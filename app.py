588b9703b3060886c58325d411428e2e5c5e0f68
Bacon <85359439+AmberBacon@users.noreply.github.com>
2021-11-06 20:38:48 -0700

    added our database model

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from models import Base, session, Book, engine
+
+
 # import models 
 # main menu - add, search, analysis, exit, view
 # add book to the database
@@ -6,3 +9,6 @@
 # search books
 # data cleaning 
 # loop runs program 
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
