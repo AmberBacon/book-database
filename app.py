e03a0d8a3e3d95cdca0b938a9f9495f68809bda3
Bacon <85359439+AmberBacon@users.noreply.github.com>
2021-11-07 00:42:49 -0700

    added book analysis

--- app.py
+++ app.py
@@ -206,7 +206,17 @@ def app():
                 time.sleep(1.5)
         elif choice == '4':
             # book anaylsis
-            pass
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%')).count()
+            print(f'''
+                  \n***** Book Analysis ******
+                  \rOldest Book: {oldest_book}
+                  \rNewest Book: {newest_book}
+                  \rTotal Books: {total_books}
+                  \rNumber of Python Books: {python_books}''')
+            input('\nPress enter to return to the main menu.')
         else: 
             print('GOODBYE')
             app_running = False
