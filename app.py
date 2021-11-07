b06710fabb3b95e7d8d92a73df9183721569a059
Bacon <85359439+AmberBacon@users.noreply.github.com>
2021-11-07 00:28:07 -0700

    created sub menu to edit, delete books

--- app.py
+++ app.py
@@ -31,6 +31,22 @@ def menu():
 # data cleaning 
 # loop runs program 
 
+def submenu(): 
+    while True:
+        print('''
+            \n1) Edit 
+            \r2) Delete
+            \r3) Return to Main Menu ''')
+        choice = input('What would you like to do?  ')
+        if choice in ['1', '2', '3']:
+            return choice
+        else:
+            input('''
+                \rPlease choose one of the menu options abvoe.
+                \rA number from 1-3.
+                \rPress enter to try again.''')
+    
+
 
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -87,6 +103,29 @@ def clean_id(id_str, options):
                 \rPress enter to try again.
                 \r*************''')
 
+def edit_check(column_name, current_value):
+    print(f'\n**** Edit {column_name} ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+        print(f'\rCurrent Value: {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+    
+    if column_name == 'Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to?  ')   
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type(changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+    else:
+        return input('What would you like to change the value to?  ')    
+    
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -149,6 +188,22 @@ def app():
                   \n{the_book.title} by {the_book.author}
                   \rPublished: {the_book.published_date}
                   \rPrice: ${the_book.price / 100}''')
+            sub_choice = submenu()
+            if sub_choice == '1':
+                # edit
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book update!')
+                time.sleep(1.5)
+            elif sub_choice == '2':
+                # delete
+                session.delete(the_book)
+                session.commit()
+                print('Book deleted!')
+                time.sleep(1.5)
         elif choice == '4':
             # book anaylsis
             pass
